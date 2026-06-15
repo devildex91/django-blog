432a5297d9deaef025f5520da530b9a88134c8a4
Neall Dexter <neall.t.dexter@gmail.com>
2026-06-15 19:28:06 +0100

    Post model added

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]',
+                 '.herokuapp.com']
+
 
 
 # Application definition
@@ -90,6 +92,10 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
