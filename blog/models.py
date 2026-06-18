1000b82981fa73ddb6e87759d2d1f22f8dc2f38d
Neall Dexter <neall.t.dexter@gmail.com>
2026-06-18 19:42:13 +0100

    models updated and generic view added with all posts

--- blog/models.py
+++ blog/models.py
@@ -17,6 +17,12 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
 
 
 class Comment(models.Model):
@@ -25,3 +31,10 @@ class Comment(models.Model):
     body = models.TextField(blank=True)
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True) 
+    
+    class Meta:
+        ordering = ["created_on"]
+    
+    def __str__(self):
+        return f"Comment {self.post} by {self.author}"
+    
