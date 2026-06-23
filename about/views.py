8bb8f75711a91135eea510d520b1ff7a062e9c59
Neall Dexter <neall.t.dexter@gmail.com>
2026-06-23 13:08:20 +0100

    about page added

---
+++ about/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views import generic
+from .models import About
+# Create your views here.
+
+
+def about_me(request):
+    """
+    Redners the about page
+    """
+    about = About.objects.all().order_by("-updated_on").first()
+    return render(
+        request,
+        "about/about.html",
+        {about: about},
+    )
